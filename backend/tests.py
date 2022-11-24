4774e91a389f068bba5203a8ee48757f95e403a8
Moxil Shah <shahmv200216@gmail.com>
2022-11-23 17:00:12 -0700

    Post tests

--- backend/tests.py
+++ backend/tests.py
@@ -34,15 +34,15 @@ class AuthorTests(APITestCase):
         author = Author.objects.get(id="ca14e1e3-77ce-44e3-8529-85172744c45b")
         self.assertEqual(author.host, HOSTNAME)
 
-    def testurl(self):
+    def testUrl(self):
         author = Author.objects.get(id="ca14e1e3-77ce-44e3-8529-85172744c45b")
-        self.assertEqual(author.url, HOSTNAME +
+        self.assertEqual(author.url(), HOSTNAME +
                          'authors/ca14e1e3-77ce-44e3-8529-85172744c45b')
         author = Author.objects.get(id="631f3ebe-d976-4248-a808-db2442a22168")
-        self.assertEqual(author.url, HOSTNAME +
+        self.assertEqual(author.url(), HOSTNAME +
                          'authors/631f3ebe-d976-4248-a808-db2442a22168')
 
-    def test_userRegistration(self):
+    def testUserRegistration(self):
         data = {
             'username': 'TestUserRegistration',
             'displayName': 'TestUserRegistration',
@@ -68,7 +68,88 @@ class AuthorTests(APITestCase):
         # becuase password too short
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+class PostsTests(APITestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        uuid_tests = [
+            "631f3ebe-d976-4248-a808-db2442a22168",
+            "ca14e1e3-77ce-44e3-8529-85172744c45b",
+        ]
+        authors = []
+
+        for i in range(len(uuid_tests)):
+            authors.append(Author.objects.create(
+                username="Author User Name" + str(i),
+                displayName="Author Display Name " + str(i),
+                id=uuid_tests[i],
+            ))
+        POST.objects.create(
+            id="631f3ebe-d976-4248-a808-db2442a22169",
+            image_url="http://127.0.0.1:8000/author/631f3ebe-d976-4248-a808-db2442a22168/post/631f3ebe-d976-4248-a808-db2442a22169",
+            title="Post Title 1",
+            source="https://github.com/CMPUT404F22T01/",
+            origin="https://github.com/CMPUT404F22T01/",
+            description="Post description 1",
+            contentType="text/plain",
+            content="Post content 1",
+            author=authors[0],
+        )
+        
+        POST.objects.create(
+            id="631f3ebe-d976-4248-a808-db2442a22167",
+            image_url="http://127.0.0.1:8000/author/ca14e1e3-77ce-44e3-8529-85172744c45b/post/631f3ebe-d976-4248-a808-db2442a22167",
+            title="Post Title 2",
+            source="https://github.com/CMPUT404F22T01/",
+            origin="https://github.com/CMPUT404F22T01/",
+            description="Post description 2",
+            contentType="text/plain",
+            content="Post content 2",
+            author=authors[1],
+        )
+
+    def testTitle(self):
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22169")
+        self.assertEqual(post.title, 'Post Title 1')
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22167")
+        self.assertEqual(post.title, 'Post Title 2')
+
+    def testImageUrl(self):
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22169")
+        self.assertEqual(post.image_url, "http://127.0.0.1:8000/" + "author/631f3ebe-d976-4248-a808-db2442a22168/post/631f3ebe-d976-4248-a808-db2442a22169")
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22167")
+        self.assertEqual(post.image_url, "http://127.0.0.1:8000/" + "author/ca14e1e3-77ce-44e3-8529-85172744c45b/post/631f3ebe-d976-4248-a808-db2442a22167")
+
+    def testSource(self):
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22169")
+        self.assertEqual(post.source, 'https://github.com/CMPUT404F22T01/')
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22167")
+        self.assertEqual(post.source, 'https://github.com/CMPUT404F22T01/')
+        
+    def testOrigin(self):
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22169")
+        self.assertEqual(post.origin, 'https://github.com/CMPUT404F22T01/')
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22167")
+        self.assertEqual(post.source, 'https://github.com/CMPUT404F22T01/')
+        
+    def testDescription(self):
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22169")
+        self.assertEqual(post.description, 'Post description 1')
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22167")
+        self.assertEqual(post.description, 'Post description 2')
+        
+    def testContent(self):
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22169")
+        self.assertEqual(post.content, 'Post content 1')
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22167")
+        self.assertEqual(post.content, 'Post content 2')
+        
+    def testAuthor(self):
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22169")
+        self.assertEqual(str(post.author.id), '631f3ebe-d976-4248-a808-db2442a22168')
+        post = POST.objects.get(id="631f3ebe-d976-4248-a808-db2442a22167")
+        self.assertEqual(str(post.author.id), 'ca14e1e3-77ce-44e3-8529-85172744c45b')
+        
 class LoginTestCase(APITestCase):
 
     def test_login(self):
